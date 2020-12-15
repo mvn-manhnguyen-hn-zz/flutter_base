a16692bb3e7798c0f15ba6a883084708273673fd
Kenny121331 <emgiaianhdai97@gmail.com>
2020-12-15 16:41:27 +0700

    fixed body

--- lib/app/page/home/views/home_view.dart
+++ lib/app/page/home/views/home_view.dart
@@ -26,14 +26,6 @@ class _HomeViewState extends ViewState<HomeView, HomeController> {
                 appBar: AppBar(
                   centerTitle: true,
                   title: Text('Title tu sua nhe'),
-                  actions: [
-                    IconButton(
-                        icon: Icon(Icons.adb),
-                        onPressed: (){
-                          Get.toNamed(Routes.RANKTOP);
-                        }
-                    )
-                  ],
                 ),
                 body: ListView.builder(
                     itemCount: controller.listShop.length,
